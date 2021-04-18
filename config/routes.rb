6a814ef64937e1053703049c4b049af0e9031858
Yu Shi <yushiys95@gmail.com>
2021-04-18 19:40:09 -0400

    seed done, api working

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :favorites
   resources :destinations
   resources :users
+  # post "/login", to: "users#login"
+  # post "/signup", to: "users#signup"
+  # get "/me", to: "users#me"
+  # patch "/me", to: "users#update"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
