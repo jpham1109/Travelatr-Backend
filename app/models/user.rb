260a84ca48bf8274ae564ff358092e6b6a44cebf
Yu Shi <yushiys95@gmail.com>
2021-04-19 09:08:37 -0400

    auth is working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :favorites
     has_many :destinations, through: :favorites 
     has_many :likes
+    has_secure_password
+    validates :username, uniqueness: { case_sensitive: false }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   resources :likes
   resources :favorites
   resources :destinations
-  resources :users
-  # post "/login", to: "users#login"
-  # post "/signup", to: "users#signup"
-  # get "/me", to: "users#me"
-  # patch "/me", to: "users#update"
+  # resources :users
+  get "/users", to: "users#index"
+  post "/login", to: "users#login"
+  post "/signup", to: "users#signup"
+  get "/me", to: "users#me"
+  patch "/me", to: "users#update"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
