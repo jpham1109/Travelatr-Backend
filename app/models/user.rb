600def4cb61fd0af192ae92105523fac1f276b73
Yu Shi <yushiys95@gmail.com>
2021-04-19 22:06:25 -0400

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-    # has_secure_password
-=======
->>>>>>> 21f811bf2dace3c307b11616cb4d5cc1e8d2ded7
     has_many :favorites
     has_many :destinations, through: :favorites 
     has_many :likes
