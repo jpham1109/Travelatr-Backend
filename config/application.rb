67984f6bd9ad9f936b3daab2b9d13a02b4cbbaed
Jamie H Pham <jpham1109@gmail.com>
2021-04-24 11:42:52 -0400

    Done

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module Travelatr
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # enable logging
+    config.logger = Logger.new(STDOUT)
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
